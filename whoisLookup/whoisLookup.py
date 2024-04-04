a0a9dde8a7e551603f690895770e19e4ad251318
User <Willspri@gmail.com>
2024-04-04 13:36:33 +1300

    features added

--- whoisLookup/whoisLookup.py
+++ whoisLookup/whoisLookup.py
@@ -2,12 +2,18 @@
 import whois
 
 #uncomment to list details of multiple sites
-#sites = ["exampl1.com", "example2.com", "example3.com"]
+#sites = ["example1.com", "example2.com", "example3.com", "example4.com", "example5.com"]
 #companies = [whois.whois(s).org for s in sites]
 #print(companies)
+
+#uncomment for emails 
+#emails = [whois.whois(s).emails for s in sites]
+
+#print(emails)
 
 res = whois.whois("example.com") #enter website here
 
 print(res)
 #uncomment and adjust to specify values to return
 #print(res.emails) 
+
